e780a601c2537056f4cafbe8d16b213203b1287a
vivek64789 <anandkushwaha2074@gmail.com>
2022-07-28 17:59:08 +0545

    updated template

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/',views.projects, name='projects'),
+    path('',views.projects, name='projects'),
     path('single-project/<str:id>/',views.project, name='singleProject'),
     path('project/update/<str:id>/',views.updateProject, name='updateProject'),
     path('project/delete/<str:id>/',views.deleteProject, name='deleteProject'),
